1324fd4318c57fe2cac0714b16dc44ad8503763c
Clement <clement@portto.com>
2022-06-23 13:31:13 +0800

    chore: remove unused scripts in fcl-sample

--- fcl-sample/build.gradle.kts
+++ fcl-sample/build.gradle.kts
@@ -11,44 +11,16 @@ setupAppModule {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles("shrinker-rules.pro", "shrinker-rules-android.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     buildFeatures {
         viewBinding = true
     }
 }
-
-//android {
-//    compileSdk = 32
-//
-//    defaultConfig {
-//        applicationId = "com.portto.fcl.sample"
-//        minSdk = 21
-//        targetSdk = 32
-//        versionCode = 1
-//        versionName = "1.0"
-//
-//        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-//    }
-//
-//    buildTypes {
-//        release {
-//            isMinifyEnabled = false
-//            proguardFiles(
-//                getDefaultProguardFile("proguard-android-optimize.txt"),
-//                "proguard-rules.pro"
-//            )
-//        }
-//    }
-//    compileOptions {
-//        sourceCompatibility = JavaVersion.VERSION_1_8
-//        targetCompatibility = JavaVersion.VERSION_1_8
-//    }
-//    kotlinOptions {
-//        jvmTarget = "1.8"
-//    }
-//}
 
 dependencies {
     implementation(projects.fclBase)
