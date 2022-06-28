de4052f753cb1a413400fbbdfba9668c6de41ee5
clement <clement@portto.com>
2022-06-28 11:31:11 +0800

    test: add unit test cases

--- fcl/src/main/java/com/portto/fcl/config/Config.kt
+++ fcl/src/main/java/com/portto/fcl/config/Config.kt
@@ -6,17 +6,23 @@ class Config() {
 
     private val configs: MutableMap<String, String> = mutableMapOf()
 
-    fun put(key: String, value: String): Config = apply { configs[key] = value }
-
     fun put(key: Key, value: String): Config = apply { configs[key.value] = value }
 
-    fun get(key: String): String? = configs[key]
+    fun put(key: String, value: String): Config = apply { configs[key] = value }
 
     fun get(key: Key): String? = configs[key.value]
+
+    fun get(key: String): String? = configs[key]
 
     fun delete(key: Key): Config = apply { configs.remove(key.value) }
 
     fun delete(key: String): Config = apply { configs.remove(key) }
+
+    fun update(key: Key, newValue: String): Config = apply { configs[key.value] = newValue }
+
+    fun update(key: String, newValue: String): Config = apply { configs[key] = newValue }
+
+    fun clear(): Config = apply { configs.clear() }
 
     override fun toString(): String {
         return configs.toString()
@@ -31,7 +37,7 @@ class Config() {
             ACCESS_NODE_KEY("accessNode.key"),
             APP_TITLE("app.detail.title"),
             APP_ICON("app.detail.icon"),
-            WALLETS("discovery.wallet"),
+//            WALLETS("discovery.wallet"),
             LIMIT("fcl.limit"),
             NETWORK("flow.network"),
             OPEN_ID_SCOPE("service.OpenID.scopes"),
--- fcl/src/test/java/com/portto/fcl/config/ConfigTest.kt
+++ fcl/src/test/java/com/portto/fcl/config/ConfigTest.kt
@@ -6,28 +6,43 @@ import com.portto.fcl.config.Config.Companion.Network.TESTNET
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  */
 class ConfigTest {
     private var config: Config = FCL.config()
 
-    @Test
-    fun putValue_returnsValue() {
-        config.put("foo", "bar")
+    @Before
+    fun setUp(){
+        config.clear()
         config.put(APP_TITLE, "Test Title")
-        config.put(NETWORK, TESTNET.type)
-
-        assertEquals("bar", config.get("foo"))
-        assertEquals("Test Title", config.get(APP_TITLE))
-        assertEquals("testnet", config.get(NETWORK))
     }
 
     @Test
-    fun delete_returnsUpdated() {
-        config.put("foo", "bar")
-        assertEquals("bar", config.get("foo"))
-        config.delete("foo")
-        assertEquals(null, config.get("foo"))
+    fun get() {
+        config.put(NETWORK, TESTNET.type)
+        config.put("config.test.t", "t")
+
+        assertEquals("Test Title", config.get(APP_TITLE))
+        assertEquals("testnet", config.get(NETWORK))
+        assertEquals("t", config.get("config.test.t"))
+    }
+
+    @Test
+    fun getNotExisted() {
+        assertEquals(null, config.get(APP_ICON))
+    }
+
+    @Test
+    fun update() {
+        config.update(APP_TITLE, "New Title")
+        assertEquals("New Title", config.get(APP_TITLE))
+    }
+
+    @Test
+    fun delete() {
+        config.delete(APP_TITLE)
+        assertEquals(null, config.get(APP_TITLE))
     }
 }
