6d266f69876116372e8fb12a03a75f9db0ddc1fa
Kihon Liu <kihon@portto.io>
2022-07-18 23:16:30 +0800

    Update Kotlin to 1.7.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,5 +9,5 @@ repositories {
 
 dependencies {
     implementation("com.android.tools.build:gradle:7.2.1")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
 }
