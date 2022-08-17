f5cf2d019ac712da4cbc7a3f64e40a417310b0bf
clement <clement@portto.com>
2022-08-17 18:35:25 +0800

    refactor: aggregate errors

--- fcl/src/main/java/com/portto/fcl/error/UnspecifiedNetworkException.kt
+++
@@ -1,6 +0,0 @@
-package com.portto.fcl.error
-
-/**
- * Exception thrown when the network is not specified
- */
-class UnspecifiedNetworkException : Exception("A Flow network must be specified")
--- fcl/src/main/java/com/portto/fcl/error/UnsupportedNetworkException.kt
+++
@@ -1,9 +0,0 @@
-package com.portto.fcl.error
-
-import com.portto.fcl.config.Network
-
-/**
- * Exception thrown when the provided [Network] is not supported
- */
-class UnsupportedNetworkException(network: Network) :
-    Exception("${network.value} is not supported by FCL")
