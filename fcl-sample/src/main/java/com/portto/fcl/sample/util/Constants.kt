d43f25b7387528b990b0c875ecf07722e9dd3ecd
clement <clement@portto.com>
2022-08-17 14:41:20 +0800

    refactor: update sample

--- fcl-sample/src/main/java/com/portto/fcl/sample/util/Constants.kt
+++ fcl-sample/src/main/java/com/portto/fcl/sample/util/Constants.kt
@@ -10,16 +10,5 @@ const val BLOCTO_APP_ID = "f10a3ec7-bfee-47aa-8c96-90024dffc9ec"
 const val FLOW_APP_IDENTIFIER = "Awesome App (v0.0)"
 const val FLOW_NONCE = "75f8587e5bd5f9dcc9909d0dae1f0ac5814458b2ae129620502cb936fde7120a"
 
-const val SCRIPT_QUERY_SAMPLE = """
-pub fun main(): Int {
-    return 1 + 2
-}
-"""
-
-const val SCRIPT = """
-transaction {
-    execute {
-        log("A transaction happened")
-    }
-}
-"""
+const val TESTNET_SAMPLE_CONTRACT_ADDRESS = "0x5a8143da8058740c"
+const val MAINNET_SAMPLE_CONTRACT_ADDRESS = "0x8320311d63f3b336"
--- fcl-sample/src/main/java/com/portto/fcl/sample/util/FlowUtils.kt
+++ fcl-sample/src/main/java/com/portto/fcl/sample/util/FlowUtils.kt
@@ -9,3 +9,28 @@ import com.portto.fcl.model.CompositeSignature
 fun List<CompositeSignature>.mapToString() = joinToString("\n\n") {
     "Address: ${it.address}\nKey ID: ${it.keyId}\nSignature: ${it.signature}"
 }
+
+
+/**
+ * Get sample script for query
+ */
+fun getQuerySampleScript(isMainnet: Boolean): String = """
+    import ValueDapp from ${if (isMainnet) MAINNET_SAMPLE_CONTRACT_ADDRESS else TESTNET_SAMPLE_CONTRACT_ADDRESS}
+
+    pub fun main(): UFix64 {
+        return ValueDapp.value
+    }
+""".trimIndent()
+
+/**
+ * Get sample script for mutate
+ */
+fun getMutateSampleScript(isMainnet: Boolean): String = """
+    import ValueDapp from ${if (isMainnet) MAINNET_SAMPLE_CONTRACT_ADDRESS else TESTNET_SAMPLE_CONTRACT_ADDRESS}
+
+    transaction(value: UFix64) {
+        prepare(authorizer: AuthAccount) {
+            ValueDapp.setValue(value)
+        }
+    }
+""".trimIndent()
