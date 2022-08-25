ee7b6d07ebd5897ecb446a02aea839dcd6d139a7
clement <clement@portto.com>
2022-08-25 17:33:58 +0800

    chore: remove unused

--- fcl/src/main/java/com/portto/fcl/utils/CoroutineUtils.kt
+++ fcl/src/main/java/com/portto/fcl/utils/CoroutineUtils.kt
@@ -1,17 +1,9 @@
 package com.portto.fcl.utils
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 internal object CoroutineUtils {
-    fun ioScope(unit: suspend () -> Unit) = CoroutineScope(Dispatchers.IO).launch { unit.invoke() }
-
-    suspend fun repeatWhen(
-        predicate: suspend () -> Boolean,
-        block: suspend () -> Unit,
-    ) {
+    suspend fun repeatWhen(predicate: suspend () -> Boolean, block: suspend () -> Unit) {
         while (predicate()) {
             block.invoke()
         }
