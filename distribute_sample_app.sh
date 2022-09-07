4428d178266056817ebf6bf9fabf1ad11d08f657
clement <clement@portto.com>
2022-09-07 23:36:03 +0800

    build: make sample app version same as library

--- distribute_sample_app.sh
+++ distribute_sample_app.sh
@@ -1,6 +1,2 @@
-file=gradle.properties
-target=VERSION_NAME=
-cur_ver=`sed -n "s/^$target\(.*\)/\1/p" < $file`
-read -p "Enter version name (current: $cur_ver): " new_ver
-sed -i "" "s/$target.*/$target$new_ver/g" $file
+#!/bin/bash
 ./gradlew clean fcl-sample:assembleRelease fcl-sample:appDistributionUploadRelease
