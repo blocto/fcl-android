73d91a88b2d8d7ed7f4dd5259d8b408eb588bca4
clement <clement@portto.com>
2022-11-12 13:03:14 +0800

    build: upgrade gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.3.0")
+    implementation("com.android.tools.build:gradle:7.3.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
 }
