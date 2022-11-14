92c552516036c40e99604efb2b413258bbdc5bc1
clement <clement@portto.com>
2022-11-14 17:45:04 +0800

    feat: update discovery wallet icons

--- fcl/src/main/java/com/portto/fcl/provider/Constants.kt
+++ fcl/src/main/java/com/portto/fcl/provider/Constants.kt
@@ -4,10 +4,10 @@ package com.portto.fcl.provider
 internal const val PROVIDER_BLOCTO_ID = 0
 internal const val PROVIDER_BLOCTO_TITLE = "Blocto"
 internal const val PROVIDER_BLOCTO_DESC = "Best wallet to interact with Web3"
-internal const val PROVIDER_BLOCTO_ICON = "https://i.imgur.com/gxQL1yq.png"
+internal const val PROVIDER_BLOCTO_ICON = "https://ipfs.blocto.app/ipfs/QmTmQQBz5KfVUcHW83S3kxqh29vSQ3cH7pcsc6cngYsG5U"
 
 // Dapper
 internal const val PROVIDER_DAPPER_ID = 1
 internal const val PROVIDER_DAPPER_TITLE = "Dapper"
 internal const val PROVIDER_DAPPER_DESC = "Wallet created by Dapper Lab"
-internal const val PROVIDER_DAPPER_ICON = "https://i.imgur.com/L1dgOKn.png"
+internal const val PROVIDER_DAPPER_ICON = "https://ipfs.blocto.app/ipfs/Qmb81oGbB9qxUct7udtHsAqiJkRf4ey2bxuDhdg1ojFDfr"
